09ba48209dc4bc05d427d5c128b403ec9b3acfee
muleudaykumar <muleudaykumar@gmail.com>
2024-12-23 11:23:17 +0530

    first commit

---
+++ dec23-class-demo-main/BankDemo.java
@@ -0,0 +1,22 @@
+
+public class BankDemo {
+
+	public static void main(String[] args) {
+	
+		BankAccount bankAcct = new BankAccount(1000, "Raju", 5000,
+				"hyd",
+				"9845098450") ;
+		
+		System.out.println(bankAcct);
+		
+		bankAcct.deposit(5000);
+		bankAcct.checkBalance();
+		
+		
+		bankAcct.withDraw(4000);
+		bankAcct.checkBalance();
+		
+		
+	}
+
+}
